c5684c84ccb345cab0c9265c0d1db8b2fe6091f0
Johan Fernandes <94102288+johanjovi17@users.noreply.github.com>
2024-07-05 13:42:03 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,24 @@ import Hero from "../components/Hero";
 import HomeCards from "../components/HomeCards";
 import JobListings from "../components/JobListings";
 import ViewAllJobs from "../components/ViewAllJobs";
+import { useUser } from "@clerk/clerk-react";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+
+  const { user } = useUser();
+  const [welcomeShown, setWelcomeShown] = useState(false);
+  useEffect(() => {
+    if (user && !welcomeShown) {
+      const hasShownWelcome = localStorage.getItem("welcomeShown");
+      if (!hasShownWelcome) {
+        toast.success(`Welcome, ${user.fullName || user.username}!`);
+        localStorage.setItem("welcomeShown", "true");
+        setWelcomeShown(true);
+      }
+    }
+  }, [user, welcomeShown]);
   return (
     <>
       <Hero />
